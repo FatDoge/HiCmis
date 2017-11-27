97d0f6002c8d5f3662a7fd37889fa577becc4948
熊吉 <704274491@qq.com>
2017-11-27 12:14:57 +0800

    fix select student module

--- js/sendNotice.js
+++ js/sendNotice.js
@@ -75,6 +75,14 @@ function commitNotice(){
 
 function selectMore(){
 	console.log("选取学生");
-	//页面层
-	window.open("./studentItems.html");
+
+	var url='./studentItems.html';                             //转向网页的地址; 
+	var name='选取学生';                            //网页名称，可为空; 
+	var iWidth=600;                          //弹出窗口的宽度; 
+	var iHeight=600;                         //弹出窗口的高度; 
+	//获得窗口的垂直位置 
+	var iTop = (window.screen.availHeight - 30 - iHeight) / 2; 
+	//获得窗口的水平位置 
+	var iLeft = (window.screen.availWidth - 10 - iWidth) / 2; 
+	window.open(url, name, 'height=' + iHeight + ',,innerHeight=' + iHeight + ',width=' + iWidth + ',innerWidth=' + iWidth + ',top=' + iTop + ',left=' + iLeft + ',status=no,toolbar=no,menubar=no,location=no,resizable=no,scrollbars=0,titlebar=no'); 
 }
